f29a17bc97377026d9127c5a0fab44b4155089ab
Pablo Cuevas <cuevasgpablo@gmail.com>
2023-10-05 17:47:59 -0300

    wip: conexion a bd

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 import express from 'express';
 import 'dotenv/config';
 import auth from './routes/auth.js';
+import { dbConnection } from './database/config.js';
 
 const PORT = process.env.PORT;
 
 // Crear app
 const app = express();
 
+// Inicializacion de BD
+dbConnection();
+
 // Generar directorio publico
 app.use(express.static('public'));
 
 // Middleware de parseo
-app.use(express.json())
+app.use(express.json());
 
 // Endpoints
 app.use('/api/auth', auth);
@@ -20,3 +24,6 @@ app.use('/api/auth', auth);
 app.listen(PORT, () => {
   console.log(`🚀 ~ Servidor corriendo en el puerto: ${PORT} ~`);
 });
+
+// mern_user
+// YdhWBxW60ENdSY0P
