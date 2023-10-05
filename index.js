bf3c76d2cbf38072222ecded5b57589dcacfb6c6
Pablo Cuevas <cuevasgpablo@gmail.com>
2023-10-05 14:51:51 -0300

    wip: endpoint del root básico

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('hola!!!!');
+import express from 'express';
+
+const PORT = 4000;
+
+const app = express();
+
+app.get('/', (req, resp) => {
+  resp.json({
+    ok: true,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 ~ Servidor corriendo en el puerto: ${PORT} ~`);
+});
