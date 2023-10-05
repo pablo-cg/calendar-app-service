186bc616b752506258e9e6924a3b2e6b286ab05b
Pablo Cuevas <cuevasgpablo@gmail.com>
2023-10-05 15:29:58 -0300

    rollback test

--- index.js
+++ index.js
@@ -7,12 +7,6 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.get('/test', (req, res) => {
-  res.json({
-    ok: true,
-  });
-});
-
 app.listen(PORT, () => {
   console.log(`🚀 ~ Servidor corriendo en el puerto: ${PORT} ~`);
 });
