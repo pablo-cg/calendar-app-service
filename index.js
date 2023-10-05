3698ac2b63c3ef338397dc877f2da3ca8139dacb
Pablo Cuevas <cuevasgpablo@gmail.com>
2023-10-05 16:32:52 -0300

    add: endpoint auth

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 import express from 'express';
 import 'dotenv/config';
+import auth from './routes/auth.js';
 
 const PORT = process.env.PORT;
 
+// Crear app
 const app = express();
 
+// Generar directorio publico
 app.use(express.static('public'));
 
+// Middleware de parseo
+app.use(express.json())
+
+// Endpoints
+app.use('/api/auth', auth);
+
+// Inicializar app
 app.listen(PORT, () => {
   console.log(`🚀 ~ Servidor corriendo en el puerto: ${PORT} ~`);
 });
