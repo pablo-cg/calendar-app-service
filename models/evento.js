ef09d716f156279493d990bcac5dbec4ae3f1a4b
Pablo Cuevas <cuevasgpablo@gmail.com>
2023-10-10 16:09:31 -0300

    add: json transformer fix: required typo

--- models/evento.js
+++ models/evento.js
@@ -3,23 +3,34 @@ import { model, Schema } from 'mongoose';
 const EventoSchema = Schema({
   title: {
     type: String,
-    require: true,
+    required: true,
   },
   notes: {
     type: String,
   },
   start: {
     type: Date,
-    require: true,
+    required: true,
   },
   end: {
     type: Date,
-    require: true,
+    required: true,
   },
   user: {
     type: Schema.Types.ObjectId,
     ref: 'Usuario',
+    required: true,
   },
 });
+
+function toJSON() {
+  const { __v, _id, ...data } = this.toObject();
+
+  data.id = _id;
+
+  return data;
+}
+
+EventoSchema.method('toJSON', toJSON);
 
 export const Evento = model('Evento', EventoSchema);
