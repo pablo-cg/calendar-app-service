cf5b004ecc36119b260839b9523f80762fe28c03
Pablo Cuevas <cuevasgpablo@gmail.com>
2023-10-10 16:10:34 -0300

    feat: crear evento

--- controllers/events.js
+++ controllers/events.js
@@ -1,4 +1,5 @@
 import { response } from 'express';
+import { Evento } from '../models/evento.js';
 
 export async function getEvents(req, resp = response) {
   resp.status(200).json({
@@ -8,10 +9,25 @@ export async function getEvents(req, resp = response) {
 }
 
 export async function createEvent(req, resp = response) {
-  resp.status(201).json({
-    ok: true,
-    msg: 'createEvent',
-  });
+  try {
+    const event = new Evento(req.body);
+    
+    event.user = req.uid;
+
+    const newEvent = await event.save();
+
+    resp.status(201).json({
+      ok: true,
+      event: newEvent,
+    });
+  } catch (error) {
+    console.log(error);
+
+    resp.status(500).json({
+      ok: false,
+      msg: 'Error inesperado...',
+    });
+  }
 }
 
 export async function updateEvent(req, resp = response) {
